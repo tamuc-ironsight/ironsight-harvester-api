6bd4eba247ad27013ef2aa72bb9a96bcd1668e48
tyleraharrison <tyleraharrison@gmail.com>
2022-02-23 02:55:41 -0600

    Initial commit
     - Needs work

---
+++ IronsightHarvesterAPI.py
@@ -0,0 +1,124 @@
+import requests
+from pprint import pprint
+
+# Make post request with JSON data
+
+
+def post_request(url, data, token):
+    response = requests.post(url, json=data, headers={
+                             'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer ' + token})
+    return response
+
+
+def createVM(vmName, claimName, imageName, userName, harvesterDomain, harvesterToken, elasticDomain, elasticToken):
+
+    jsonData = {
+        "apiVersion": "kubevirt.io/v1",
+        "kind": "VirtualMachine",
+        "metadata": {
+            "namespace": "default",
+            "annotations": {
+                "harvesterhci.io/volumeClaimTemplates": "[{\"metadata\":{\"name\":\"" + claimName + "\",\"annotations\":{\"harvesterhci.io/imageId\":\"default/" + imageName + "\"}},\"spec\":{\"accessModes\":[\"ReadWriteMany\"],\"resources\":{\"requests\":{\"storage\":\"25Gi\"}},\"volumeMode\":\"Block\",\"storageClassName\":\"longhorn-image-vk9x6\"}}]",
+                "network.harvesterhci.io/ips": "[]"
+            },
+            "labels": {
+                "harvesterhci.io/creator": "harvester",
+                "harvesterhci.io/os": "linux"
+            },
+            "name": vmName
+        },
+        "__clone": True,
+        "spec": {
+            "running": True,
+            "template": {
+                "metadata": {
+                    "annotations": {
+                        "harvesterhci.io/sshNames": "[]"
+                    },
+                    "labels": {
+                        "harvesterhci.io/vmName": "test-api"
+                    }
+                },
+                "spec": {
+                    "domain": {
+                        "machine": {
+                            "type": "q35"
+                        },
+                        "cpu": {
+                            "cores": 4,
+                            "sockets": 1,
+                            "threads": 1
+                        },
+                        "devices": {
+                            "inputs": [],
+                            "interfaces": [
+                                {
+                                    "masquerade": {},
+                                    "model": "virtio",
+                                    "name": "default"
+                                }
+                            ],
+                            "disks": [
+                                {
+                                    "name": "disk-0",
+                                    "disk": {
+                                        "bus": "virtio"
+                                    },
+                                    "bootOrder": 1
+                                },
+                                {
+                                    "name": "cloudinitdisk",
+                                    "disk": {
+                                        "bus": "virtio"
+                                    }
+                                }
+                            ]
+                        },
+                        "resources": {
+                            "limits": {
+                                "memory": "2Gi",
+                                "cpu": 4
+                            }
+                        }
+                    },
+                    "evictionStrategy": "LiveMigrate",
+                    "hostname": "test-api",
+                    "networks": [
+                        {
+                            "pod": {},
+                            "name": "default"
+                        }
+                    ],
+                    "volumes": [
+                        {
+                            "name": "disk-0",
+                            "persistentVolumeClaim": {
+                                "claimName": claimName
+                            }
+                        },
+                        {
+                            "name": "cloudinitdisk",
+                            "cloudInitConfigDrive": {
+                                "userData" : 	"#cloud-config\npackage_update: true\npackages:\n  - wget\nhostname: "+ vmName + "\nusers:\n  - default\n  - name: " + userName + "\n    gecos: Student User\n    expiredate: '2032-09-01'\n    lock_passwd: false\n    passwd: $6$rounds=4096$Vd8W45YhfEELz1sq$HVp7eLIeJM.XOmN8o.RAwrg1UsqKpAXZBClx6uSX46j5Jwe4HN7cPdPYaKDLUVKYcAvjGTyRP3w26OrIo/.HD1\nruncmd:\n  - [ mkdir, /home/elastic ]\n  - [ wget, \"https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.16.3-linux-x86_64.tar.gz\", -O, /home/elastic/agent.tar.gz ]\n  - [ tar, -xvf, /home/elastic/agent.tar.gz, -C, /home/elastic/ ]\n  - [ ./home/elastic/elastic-agent-7.16.3-linux-x86_64/elastic-agent, install, \"-f\",\"--url=" + elasticDomain + "\", \"--enrollment-token=" + elasticToken + "\", \"--insecure\" ]\n  - [\"touch\", \"/etc/cloud/cloud-init.disabled\"]"
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+    }
+
+    print("VM Name: " + vmName)
+    print("Claim Name: " + claimName)
+    print("Image Name: " + imageName)
+    print("User Name: " + userName)
+    print("Harvester Domin: " + harvesterDomain.split('apis')[0])
+    print("Elastic Domin: " + elasticDomain)
+    print("Creating VM...")
+    postResponse = post_request(harvesterDomain, jsonData, harvesterToken)
+    print(postResponse.status_code)
+    if postResponse.status_code == 201:
+        print("VM Created Successfully")
+    else:
+        print("Error creating VM")
+        pprint(postResponse.text.strip())
---
+++ createVM.py
@@ -0,0 +1,57 @@
+from IronsightHarvesterAPI import createVM
+import random
+import string
+import sys
+import openpyxl
+from pprint import pprint
+
+def listTemplates():
+    print("VM Templates:\n----------------")
+    for template in templates:
+        print(" - " + template['normalName'] + ": " + template['description'])
+
+if __name__ == "__main__":
+
+    templates = []
+    # Get normalName, vmName, image name, and description from excel file
+    templateSheet = openpyxl.load_workbook('TemplateList.xlsx')
+    # templateSheet is the first sheet (Use wb[sheetname] to get the sheet)
+    templateSheet = templateSheet[templateSheet.sheetnames[0]]
+    for row in range(2, templateSheet.max_row + 1):
+        templates.append({
+            'normalName': templateSheet.cell(row=row, column=1).value,
+            'vmName': templateSheet.cell(row=row, column=2).value,
+            'imageName': templateSheet.cell(row=row, column=3).value,
+            'description': templateSheet.cell(row=row, column=4).value
+        })
+
+    if len(sys.argv) < 4:
+        print("Usage: python3 createVM.py [vmName] [template] [studentName]")
+        sys.exit(1)
+    if "--templates" in sys.argv:
+        listTemplates()
+        sys.exit(0)
+    
+    vmName = sys.argv[1]
+    imageName = ""
+    try:
+        for template in templates:
+            if template['normalName'] == sys.argv[2]:
+                imageName = template['normalName']
+                break
+        if imageName == "":
+            raise Exception("Invalid template")
+    except:
+        print("Template not found. Here are the available templates:")
+        listTemplates()
+        sys.exit(1)
+    userName = sys.argv[3]
+    harvesterToken = "token-llcfk:dxqlvcxglvg744z2hlpvwgjfqkcxlsczprkbtg4vrxqckgdbq7z7cg"
+    harvesterDomain = "https://vm.tylerharrison.dev/apis/kubevirt.io/v1/namespaces/default/virtualmachines"
+    elasticDomain = "http://ssh.tylerharrison.dev:8220"
+    elasticToken = "a1VJbXBYNEJZOEUtRnJyRlhvc0Q6U3VFZGRPcldUcW0tRzMyX2s3UmtTUQ=="
+    vmName = vmName + "-" + userName
+    randomLetters = "".join(random.choice(
+    string.ascii_lowercase) for i in range(5))
+    claimName = vmName + "-claim" + randomLetters
+    createVM(vmName, claimName, imageName, userName, harvesterDomain, harvesterToken, elasticDomain, elasticToken)
