6a5f821c15d5e06bbdbd7c879bc6d33d5aa15ad0
tyleraharrison <tyleraharrison@gmail.com>
2022-03-18 03:15:16 -0500

    Moved create function to API

--- createVM.py
+++ createVM.py
@@ -1,14 +1,10 @@
-from ironsight_harvester_api import createVM
+from ironsight_harvester_api import createVM, listTemplates
 import random
 import string
 import sys
 import json
 import ironsight_sql
 from pprint import pprint
-
-def listTemplates():
-    templatesJSON = ironsight_sql.query("SELECT * FROM templates", sql_server, sql_user, sql_pass, sql_db)
-    ironsight_sql.pretty_response(templatesJSON)
 
 if __name__ == "__main__":
 
@@ -19,42 +15,8 @@ if __name__ == "__main__":
     if len(sys.argv) < 4:
         print("Usage: python3 createVM.py [vmName] [template] [studentName]")
         sys.exit(1)
-    
-    # Load in configuration file
-    with open('config.json') as config_file:
-        config = json.load(config_file)
-        sql_server = config['sql_server']
-        sql_user = config['sql_user']
-        sql_pass = config['sql_pass']
-        sql_db = config['sql_db']
-        harvester_token = config['harvester_api_token']
-        harvester_url = config['harvester_api_url']
-        elastic_url = config['elasticsearch_url']
-        elastic_token = config['elasticsearch_api_token']
-
-    # Load in templates from SQL
-    templatesJSON = ironsight_sql.query("SELECT * FROM templates", sql_server, sql_user, sql_pass, sql_db)
-
-    # Make sure template exists in SQL, if so, get image name and image size (in gigabytes)
-    # Otherwise, print error and exit
-    imageName = ""
-    volumeSize = ""
-    try:
-        for template in templatesJSON:
-            if str(sys.argv[2]) == str(template['template_name']):
-                imageName = str(template['template_image'])
-                volumeSize = str(template['template_volume_size'])
-    except:
-        print("Template not found. Here are the available templates:")
-        listTemplates()
-        sys.exit(1)
-
-    # Configure VM properties
+    vmName = str(sys.argv[1])
+    template_choice = str(sys.argv[2])
     userName = str(sys.argv[3])
-    vmName = str(sys.argv[1]) + "-" + userName
-    randomLetters = "".join(random.choice(
-    string.ascii_lowercase) for i in range(5))
-    claimName = vmName + "-claim" + randomLetters
-
     # Create virtual machine
-    createVM(vmName, claimName, imageName, volumeSize, userName, harvester_url, harvester_token, elastic_url, elastic_token)
+    createVM(vmName, template_choice, userName)
--- ironsight_harvester_api.py
+++ ironsight_harvester_api.py
@@ -2,6 +2,22 @@ import requests
 import urllib3
 from pprint import pprint
 import ironsight_sql
+import json
+import random
+import string
+import sys
+
+# Load in configuration file
+with open('config.json') as config_file:
+    config = json.load(config_file)
+    sql_server = config['sql_server']
+    sql_user = config['sql_user']
+    sql_pass = config['sql_pass']
+    sql_db = config['sql_db']
+    harvester_token = config['harvester_api_token']
+    harvester_url = config['harvester_api_url']
+    elastic_url = config['elasticsearch_url']
+    elastic_token = config['elasticsearch_api_token']
 
 # Make post request with JSON data
 def post_request(url, data, token):
@@ -10,8 +26,33 @@ def post_request(url, data, token):
                              'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer ' + token})
     return response
 
+def listTemplates():
+    templatesJSON = ironsight_sql.query("SELECT * FROM templates", sql_server, sql_user, sql_pass, sql_db)
+    ironsight_sql.pretty_response(templatesJSON)
 
-def createVM(vmName, claimName, imageName, volumeSize, userName, harvesterDomain, harvesterToken, elasticDomain, elasticToken):
+def createVM(vmName, template_choice, userName):
+    # Load in templates from SQL
+    templatesJSON = ironsight_sql.query("SELECT * FROM templates", sql_server, sql_user, sql_pass, sql_db)
+
+    # Make sure template exists in SQL, if so, get image name and image size (in gigabytes)
+    # Otherwise, print error and exit
+    imageName = ""
+    volumeSize = ""
+    try:
+        for template in templatesJSON:
+            if template_choice == str(template['template_name']):
+                imageName = str(template['template_image'])
+                volumeSize = str(template['template_volume_size'])
+    except:
+        print("Template not found. Here are the available templates:")
+        listTemplates()
+        sys.exit(1)
+
+    # Configure VM properties
+    vmName = vmName + "-" + userName
+    randomLetters = "".join(random.choice(
+    string.ascii_lowercase) for i in range(5))
+    claimName = vmName + "-claim" + randomLetters
 
     jsonData = {
         "apiVersion": "kubevirt.io/v1",
@@ -100,7 +141,7 @@ def createVM(vmName, claimName, imageName, volumeSize, userName, harvesterDomain
                         {
                             "name": "cloudinitdisk",
                             "cloudInitConfigDrive": {
-                                "userData" : 	"#cloud-config\npackage_update: true\npackages:\n  - wget\nhostname: "+ vmName + "\nusers:\n  - default\n  - name: " + userName + "\n    gecos: Student User\n    expiredate: '2032-09-01'\n    lock_passwd: false\n    passwd: $6$rounds=4096$Vd8W45YhfEELz1sq$HVp7eLIeJM.XOmN8o.RAwrg1UsqKpAXZBClx6uSX46j5Jwe4HN7cPdPYaKDLUVKYcAvjGTyRP3w26OrIo/.HD1\nruncmd:\n  - [ mkdir, /home/elastic ]\n  - [ wget, \"https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.16.3-linux-x86_64.tar.gz\", -O, /home/elastic/agent.tar.gz ]\n  - [ tar, -xvf, /home/elastic/agent.tar.gz, -C, /home/elastic/ ]\n  - [ ./home/elastic/elastic-agent-7.16.3-linux-x86_64/elastic-agent, install, \"-f\",\"--url=" + elasticDomain + "\", \"--enrollment-token=" + elasticToken + "\", \"--insecure\" ]\n  - [\"touch\", \"/etc/cloud/cloud-init.disabled\"]"
+                                "userData" : 	"#cloud-config\npackage_update: true\npackages:\n  - wget\nhostname: "+ vmName + "\nusers:\n  - name: " + userName + "\n    gecos: Student User\n    expiredate: '2032-09-01'\n    lock_passwd: false\n    passwd: $6$rounds=4096$Vd8W45YhfEELz1sq$HVp7eLIeJM.XOmN8o.RAwrg1UsqKpAXZBClx6uSX46j5Jwe4HN7cPdPYaKDLUVKYcAvjGTyRP3w26OrIo/.HD1\nruncmd:\n  - [ mkdir, /home/elastic ]\n  - [ wget, \"https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.16.3-linux-x86_64.tar.gz\", -O, /home/elastic/agent.tar.gz ]\n  - [ tar, -xvf, /home/elastic/agent.tar.gz, -C, /home/elastic/ ]\n  - [ ./home/elastic/elastic-agent-7.16.3-linux-x86_64/elastic-agent, install, \"-f\",\"--url=" + elastic_url + "\", \"--enrollment-token=" + elastic_token + "\", \"--insecure\" ]\n  - [\"touch\", \"/etc/cloud/cloud-init.disabled\"]"
                             }
                         }
                     ]
@@ -113,10 +154,10 @@ def createVM(vmName, claimName, imageName, volumeSize, userName, harvesterDomain
     print("Claim Name: " + claimName)
     print("Image Name: " + imageName)
     print("User Name: " + userName)
-    print("Harvester Domin: " + harvesterDomain.split('apis')[0])
-    print("Elastic Domin: " + elasticDomain)
+    print("Harvester Domin: " + harvester_url.split('apis')[0])
+    print("Elastic Domin: " + elastic_url)
     print("Creating VM...")
-    postResponse = post_request(harvesterDomain, jsonData, harvesterToken)
+    postResponse = post_request(harvester_url, jsonData, harvester_token)
     print(postResponse.status_code)
     if postResponse.status_code == 201:
         print("VM Created Successfully")
