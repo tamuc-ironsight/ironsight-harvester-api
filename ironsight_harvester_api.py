c3ad69e03331abf6e83b2231c2c9ea1974557c40
tyleraharrison <tyleraharrison@gmail.com>
2022-04-10 00:04:53 -0500

    Removed ports from IP

--- ironsight_harvester_api.py
+++ ironsight_harvester_api.py
@@ -393,7 +393,13 @@ def get_metrics():
 def get_cpu_usage(start_time, end_time, step):
     query_url = harvester_url + f"/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/api/datasources/proxy/1/api/v1/query_range?query=1%20-%20avg(irate(%7B__name__%3D~%22node_cpu_seconds_total%7Cwindows_cpu_time_total%22%2Cmode%3D%22idle%22%7D%5B240s%5D))%20by%20(instance)&start={start_time}&end={end_time}&step={step}"
     getResponse = get_request(query_url, harvester_token)
-    print(getResponse.text)
+    # Remove the port number from the instance name
+    response = json.loads(getResponse.text)
+    for i in range(len(response['data']['result'])):
+        instance = response['data']['result'][i]['metric']['instance']
+        instance = instance.split(":")[0]
+        response['data']['result'][i]['metric']['instance'] = instance
+    print(json.dumps(response))
     sys.exit(1)
 
 def get_network_usage(start_time, end_time, step):
@@ -437,7 +443,13 @@ def get_network_usage(start_time, end_time, step):
 def get_memory_usage(start_time, end_time, step):
     query_url = harvester_url + f"/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/api/datasources/proxy/1/api/v1/query_range?query=1%20-%20sum(node_memory_MemAvailable_bytes%20OR%20windows_os_physical_memory_free_bytes)%20by%20(instance)%20%2F%20sum(node_memory_MemTotal_bytes%20OR%20windows_cs_physical_memory_bytes)%20by%20(instance)%20&start={start_time}&end={end_time}&step={step}"
     getResponse = get_request(query_url, harvester_token)
-    print(getResponse.text)
+    # Remove the port number from the instance name
+    response = json.loads(getResponse.text)
+    for i in range(len(response['data']['result'])):
+        instance = response['data']['result'][i]['metric']['instance']
+        instance = instance.split(":")[0]
+        response['data']['result'][i]['metric']['instance'] = instance
+    print(json.dumps(response))
     sys.exit(1)
 
 def get_disk_usage(start_time, end_time, step):
